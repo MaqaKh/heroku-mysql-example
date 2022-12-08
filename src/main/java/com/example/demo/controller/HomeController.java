cc8a09d5d03c650c99f08be5a1d92b5d1c32b6d8
MaqaKh <khalilovmaga05@gmail.com>
2022-12-09 01:47:24 +0400

    remove files

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,6 +1,5 @@
 package com.example.demo.controller;
 
-import com.example.demo.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,21 +7,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
-    @Autowired
-    Service service;
 
     @Autowired
     UserRepository userRepository;
 
     @GetMapping("/")
-    public String trigegr(){
+    public String trigegr() {
         User user = new User("john");
         userRepository.save(user);
         return user.id;
     }
 
     @GetMapping("/bbb")
-    public String trigegr2(){
+    public String trigegr2() {
         System.out.println("bbb thread:" + Thread.currentThread().getName());
         return "bbb";
     }
